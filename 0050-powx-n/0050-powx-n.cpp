001ed6a477856e8b7a7995da28775029ef9c3b7e
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-03 04:22:16 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     double multiply(double x, long long n){
-      if(n == 0){
-        return 1;
-      }
+        if(n == 0){
+            return 1;
+        }
+        double half = multiply(x, n/2);
 
-      double half = multiply(x, n/2);
-      if(n % 2 == 0){
-        return half * half;
-      }
+        if(n %  2 == 0){
+            return half * half;
+        }
 
-      return x * half * half;
+        return half * half * x;
     }
-    double myPow(double x, long long n) {
+    double myPow(double x, int n) {
         long long N = n;
         if(N < 0){
-            return 1.0/multiply(x, -N);
+            return 1.0/multiply(x, N);
         }
         return multiply(x, N);
+
+        
     }
 };
