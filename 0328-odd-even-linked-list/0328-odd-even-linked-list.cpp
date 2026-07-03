d32e645fdf1673e27acd4cd20fed5da256978812
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-04 03:06:27 +0530

    Time: 0 ms (100%), Space: 15.6 MB (27.97%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -14,18 +14,20 @@ public:
         if(head == NULL || head->next == NULL){
             return head;
         }
+
         ListNode* odd = head;
-        ListNode* evenHead = head->next;
+        ListNode* oddHead = odd;
         ListNode* even = head->next;
+        ListNode* evenHead = even;
 
         while(even != NULL && even->next != NULL){
-            odd->next = odd->next->next;
-            even->next = even->next->next;
+            odd->next = even->next;
             odd = odd->next;
+            even->next = odd->next;
             even = even->next;
         }
+
         odd->next = evenHead;
-      
         return head;
     }
 };
