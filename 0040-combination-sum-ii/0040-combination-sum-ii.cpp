cce89d19259352636560200b61dd1b025e507b85
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-11 19:47:32 -0700

    Time: 0 ms (100%), Space: 14.1 MB (63.92%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void reversal(vector<vector<int>> &ans, vector<int> &curr, vector<int> &candidates, int target, int idx){
+        if(target == 0){
+            ans.push_back(curr);
+            return;
+        }
+        if(idx == candidates.size()){
+            return;
+        }
+
+        for(int i = idx; i < candidates.size(); i++){
+            if(i > idx && candidates[i] == candidates[i - 1]) continue;
+
+            if(target < candidates[i]) break;
+
+            curr.push_back(candidates[i]);
+            reversal(ans, curr, candidates, target - candidates[i], i + 1);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> curr;
+        reversal(ans, curr, candidates, target, 0);
+        return ans;
+
+    }
+};
