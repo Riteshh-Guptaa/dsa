393f51b623b7254222ba305a5c83aa4515255987
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-15 08:19:07 -0700

    Time: 20 ms (7.18%), Space: 23 MB (49.28%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,24 +1,27 @@
 class Solution {
-public:  
-    long long timeFinder(vector<int> &piles, int mid){
+public:
+    long long finder(int mid, vector<int> &piles){
         long long cnt = 0;
-        for(int i = 0; i < piles.size(); i++){
-            cnt += ceil((double)piles[i]/mid);
+        for(int num : piles){
+            cnt += ceil(double(num)/mid);
+
         }
         return cnt;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int low = 1;
         int high = *max_element(piles.begin(), piles.end());
+
         if(h < piles.size()) return -1;
 
         while(low <= high){
             int mid = low + (high - low)/2;
 
-            if(timeFinder(piles, mid) <= h){
-                high = mid - 1;
-            }else{
+            if(finder(mid, piles) > h){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
         return low;
