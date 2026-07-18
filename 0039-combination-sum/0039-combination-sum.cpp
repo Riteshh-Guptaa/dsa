99eb2cdaea12d8bd1348f1dc41a29f43fea9fca8
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-18 03:35:55 -0700

    Time: 3 ms (52.76%), Space: 14.1 MB (76.48%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solver(vector<int>& candidates, vector<vector<int>>& ans, vector<int> &curr, int target, int idx){
+    void reversal(vector<int> &candidates, vector<vector<int>> &ans, vector<int> &curr, int target, int idx){
         if(target == 0){
             ans.push_back(curr);
             return;
@@ -12,15 +12,17 @@ public:
 
         if(target >= candidates[idx]){
             curr.push_back(candidates[idx]);
-            solver(candidates, ans, curr, target - candidates[idx], idx);
+            reversal(candidates, ans, curr, target - candidates[idx], idx);
             curr.pop_back();
         }
-        solver(candidates, ans, curr, target, idx + 1);
+
+        reversal(candidates, ans, curr, target, idx + 1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> curr;
-        solver(candidates, ans, curr, target, 0);
-        return ans;
+
+        reversal(candidates, ans, curr, target, 0);
+        return ans; 
     }
 };
