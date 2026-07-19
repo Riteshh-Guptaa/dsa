11fe1ce32e0089bb678ff7cf66a0d9fc9e5c3f01
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-19 04:33:54 -0700

    Time: 0 ms (100%), Space: 10.9 MB (32.89%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void reversal(vector<vector<int>>& ans, vector<int>& curr, vector<int>& nums, int idx){
+    void reversal(vector<vector<int>> &ans, vector<int> &curr, vector<int> nums, int idx){
         ans.push_back(curr);
 
         for(int i = idx; i < nums.size(); i++){
             if(i > idx && nums[i] == nums[i - 1]) continue;
+
             curr.push_back(nums[i]);
             reversal(ans, curr, nums, i + 1);
             curr.pop_back();
