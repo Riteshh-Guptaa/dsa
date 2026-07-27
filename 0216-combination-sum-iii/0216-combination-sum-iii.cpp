3726ff1a812d23a0c8c8d9e0fd75eb3a68f9d4af
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-27 09:30:33 -0700

    Time: 0 ms (100%), Space: 8.6 MB (82.99%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    void reversal(vector<vector<int>> &ans, vector<int> &curr, int k, int n, int idx){
-        if(curr.size() == k){
+    void reversal(int k, int n, vector<vector<int>> &ans, vector<int> &curr, int idx){
+        if(k == curr.size()){
             if(n == 0){
                 ans.push_back(curr);
-                return;
+                
             }
+            return;
         }
 
         for(int i = idx; i <= 9; i++){
+
             if(i > n) break;
             curr.push_back(i);
-            reversal(ans, curr, k, n - i, i + 1);
+            reversal(k, n - i, ans, curr, i + 1);
             curr.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> curr;
-        reversal(ans, curr, k, n, 1);
+        reversal(k, n, ans, curr, 1);
         return ans;
     }
 };
