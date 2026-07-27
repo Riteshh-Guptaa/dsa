d6be80f6b197e5ca6496d95a00c3589c18074de2
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-27 11:42:24 -0700

    Time: 0 ms (100%), Space: 9.9 MB (7.42%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    void reversal(vector<string> &ans, string digits, string curr, vector<string> map, int idx){
+    void reversal(vector<string> &ans, string &curr, vector<string> map, string digits, int idx){
         if(idx == digits.size()){
             ans.push_back(curr);
             return;
         }
-
-        string letters = map[digits[idx] - '0'];
-
-        for(char letter : letters){
-            curr.push_back(letter);
-            reversal(ans, digits, curr, map, idx + 1);
-            curr.pop_back();
-        }
+         string letters = map[digits[idx] - '0'];
+    for(char letter : letters){
+        curr.push_back(letter);
+        reversal(ans, curr, map, digits, idx + 1);
+        curr.pop_back();
     }
+    }
+
+
+   
+
     vector<string> letterCombinations(string digits) {
+        vector<string> ans;
+        string curr = "";
         vector<string> map = {
             "",
             ",",
@@ -28,9 +32,7 @@ public:
             "wxyz"
         };
 
-        vector<string> ans;
-        string curr = "";
-        reversal(ans, digits, curr, map, 0);
+        reversal(ans, curr, map, digits, 0);
         return ans;
     }
 };
