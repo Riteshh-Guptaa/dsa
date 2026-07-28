52b87357ea5d83ced306d80007fff1b43c47fa85
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-28 10:26:38 -0700

    Time: 3 ms (61.64%), Space: 10.2 MB (79.15%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void reversal(int col, vector<string> &board, vector<vector<string>> &ans, vector<int> &lowerDiog, vector<int> &rowChecker, vector<int> &upperDiog, int n){
+    void reversal(vector<vector<string>> &ans, vector<string> &board, int col, vector<int> &rowChecker, vector<int> &lowerDiog, vector<int> &upperDiog, int n){
         if(col == n){
             ans.push_back(board);
             return;
         }
 
         for(int row = 0; row < n; row++){
-            if(rowChecker[row] == 0 && upperDiog[n - 1 + col - row] == 0 && lowerDiog[row + col] == 0 ){
+            if(rowChecker[row] == 0 && lowerDiog[row + col] == 0 && upperDiog[n - 1 + col - row] == 0){
                 board[row][col] = 'Q';
-                rowChecker[row] = 1;
-                upperDiog[n - 1 + col - row] = 1;
-                lowerDiog[row + col] = 1;
-                reversal(col + 1, board, ans, lowerDiog, rowChecker, upperDiog, n);
-                board[row][col] = '.';
+                rowChecker[row]++;
+                lowerDiog[row + col]++;
+                upperDiog[n - 1 + col - row]++;
+                reversal(ans, board, col + 1, rowChecker, lowerDiog, upperDiog, n);
+                board[row][col] ='.';
                 rowChecker[row] = 0;
-                upperDiog[n - 1 + col - row] = 0;
                 lowerDiog[row + col] = 0;
+                upperDiog[n - 1 + col - row] = 0;
             }
         }
     }
@@ -24,11 +24,16 @@ public:
         vector<vector<string>> ans;
         vector<string> board(n);
         string s(n, '.');
+
         for(int i = 0; i < n; i++){
-            board[i] = s; 
+            board[i] = s;
         }
-        vector<int> rowChecker(n, 0), upperDiog(2 * n - 1, 0), lowerDiog(2 * n - 1, 0);
-        reversal(0, board, ans, lowerDiog, rowChecker, upperDiog, n);
+
+        vector<int> rowChecker(n, 0);
+        vector<int> lowerDiog(2 * n - 1, 0);
+        vector<int> upperDiog(2 * n - 1, 0);
+
+        reversal(ans, board, 0, rowChecker, lowerDiog, upperDiog, n);
         return ans;
     }
 };
