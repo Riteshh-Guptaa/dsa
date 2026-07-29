02fbe106a9daf1caed96a88ae7cb77f3882464e4
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-07-29 04:15:28 -0700

    Time: 441 ms (18.35%), Space: 93.5 MB (28.12%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,51 +1,35 @@
 class Solution {
 public:
-    void reversal(string num, int target, int pos,
-                  string path, vector<string> &ans,
-                  long long curr, long long prev) {
-
-        if (pos == num.size()) {
-            if (curr == target)
-                ans.push_back(path);
+    void reversal(vector<string> &ans, string num, int target, string move, long long curr, long long prev, int idx){
+        if(idx == num.size()){
+            if(curr == target){
+                ans.push_back(move);
+            }
             return;
         }
 
-        for (int i = pos; i < num.size(); i++) {
-
-            if (i > pos && num[pos] == '0')
+        for(int i = idx; i < num.size(); i++){
+            if(i > idx && num[idx] == '0'){
                 break;
+            }
 
-            string s = num.substr(pos, i - pos + 1);
+            string s = num.substr(idx, i - idx + 1);
             long long val = stoll(s);
 
-            if (pos == 0) {
-                reversal(num, target, i + 1, s, ans, val, val);
-            } else {
+            if(idx == 0){
+                reversal(ans, num, target, s, val, val, i + 1);
+            }else{
+                reversal(ans, num, target, move + "+" + s, curr + val, val, i + 1);
 
-                reversal(num, target, i + 1,
-                         path + "+" + s,
-                         ans,
-                         curr + val,
-                         val);
+                reversal(ans, num, target, move + "-" + s, curr - val, -val, i + 1);
 
-                reversal(num, target, i + 1,
-                         path + "-" + s,
-                         ans,
-                         curr - val,
-                         -val);
-
-                reversal(num, target, i + 1,
-                         path + "*" + s,
-                         ans,
-                         curr - prev + prev * val,
-                         prev * val);
+                reversal(ans, num, target, move + "*" + s, curr - prev + prev * val, prev * val, i + 1);
             }
         }
     }
-
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
-        reversal(num, target, 0, "", ans, 0, 0);
+        reversal(ans, num, target, "", 0, 0, 0);
         return ans;
     }
 };
