6ff8cff8c4f58f0451ef6ebc325d9809864ba2d5
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-03 09:35:05 -0700

    Time: 0 ms (100%), Space: 95.1 MB (62.05%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -4,36 +4,32 @@ public:
         int n1 = nums1.size();
         int n2 = nums2.size();
         if(n1 > n2) return findMedianSortedArrays(nums2, nums1);
-
-        int low = 0;
-        int left = (n1 + n2 + 1)/2;
-        int high = min(left, n1);
+        int k = (n1 + n2 + 1)/2;
+        int low = max(0, k - n2);
+        int high = min(n1, k);
+        
 
         while(low <= high){
             int mid1 = low + (high - low)/2;
-            int mid2 = left - mid1;
+            int mid2 = k - mid1;
 
-            int l1 = (mid1 == 0) ? INT_MIN : nums1[mid1 - 1];
-            int l2 = (mid2 == 0) ? INT_MIN : nums2[mid2 - 1];
+            int l1 = (mid1 > 0) ? nums1[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
+            int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
 
-            int r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
-            int r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
-
-            if(r2 >= l1 && l2 <= r1){
+            if(l1 <= r2 && l2 <= r1){
                 if((n1 + n2) % 2 == 0){
                     return (max(l1, l2) + min(r1, r2))/2.0;
                 }else{
                     return max(l1, l2);
                 }
-            }
-
-            if(l1 > r2){
+            }else if(l1 > r2){
                 high = mid1 - 1;
             }else{
                 low = mid1 + 1;
             }
         }
-    return -1;
-
+        return -1;
     }
 };
