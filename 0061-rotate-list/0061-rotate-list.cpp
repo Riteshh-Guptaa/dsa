b318a0283b134f757b7a6c97002e99759ef0fdfd
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-06 05:56:07 -0700

    Time: 0 ms (100%), Space: 16.3 MB (95.67%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -11,33 +11,45 @@
 class Solution {
 public:
     int count(ListNode* head){
+        ListNode* curr = head;
         int cnt = 0;
-        while(head){
+        while(curr){
             cnt++;
-            head= head->next;
+            curr = curr->next;
         }
         return cnt;
+    }
+
+    ListNode* nodeFinder(ListNode* head, int req){
+        ListNode* curr = head;
+        int cnt = 1;
+        while(curr != NULL && cnt < req){
+            curr = curr->next;
+            cnt++;
+        }
+        return curr;
     }
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL || k == 0){
             return head;
         }
+
         int cnt = count(head);
         k %= cnt;
         if(k == 0) return head;
+
         int req = cnt - k;
-        ListNode* temp = head;
-        for(int i = 1; i < req; i++){
+        ListNode* newNode = nodeFinder(head, req);
+        ListNode* newHead = newNode->next;
+        ListNode* temp = newHead;
+        newNode->next = NULL;
+
+        while(temp->next){
             temp = temp->next;
         }
+        temp->next = head;
+        return newHead;
 
-        ListNode* newhead = temp->next;
-        ListNode* curr = newhead;
-        temp->next = NULL;
-        while(curr->next){
-            curr = curr->next;
-        }
-        curr->next = head;
-        return newhead;
+
     }
 };
