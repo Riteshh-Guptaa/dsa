6743eaabb3cfe9edc57f56110f9227ec70080a96
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-07 00:52:30 -0700

    Time: 0 ms (100%), Space: 8.7 MB (44.55%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    double reversal(double x, long long N){
-        if(N == 0){
-            return 1.00;
+    double reversal(double x, long int n){
+        if(n == 0){
+            return 1.0;
         }
 
-        double half = reversal(x, N/2);
-        if(N % 2 == 1){
-           return  x * half * half;
-        }else{
+        double half = reversal(x, n/2);
+        if(n % 2 == 0){
             return half * half;
+        }else{
+            return x * half * half;
         }
+
     }
     double myPow(double x, int n) {
         long long N = n;
