4f7dde0d6af16a64d21b71a8a9199e2e01fefe3a
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-08 08:10:09 -0700

    Time: 0 ms (100%), Space: 95.1 MB (80.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -13,7 +13,7 @@ public:
             int mid2 = k - mid1;
 
             int l1 = (mid1 > 0) ? nums1[mid1 - 1] : INT_MIN;
-            int l2 = (mid2> 0) ? nums2[mid2 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? nums2[mid2 - 1] : INT_MIN;
             int r1 = (mid1 < n1) ? nums1[mid1] : INT_MAX;
             int r2 = (mid2 < n2) ? nums2[mid2] : INT_MAX;
 
@@ -23,14 +23,13 @@ public:
                 }else{
                     return max(l1, l2);
                 }
-            } 
-                else if(l1 > r2){
-                    high = mid1 - 1;
-                }else{
-                    low = mid1 + 1;
-                }
-            
+            }else if(l1 > r2){
+                high = mid1 - 1;
+            }else{
+                low = mid1 + 1;
+            }
         }
-        return -1;
+          return -1;
     }
+  
 };
