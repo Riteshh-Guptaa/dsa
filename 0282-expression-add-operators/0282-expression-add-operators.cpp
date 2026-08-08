cf7c0a100da687b10c1ca9d9b8d02cfaef20f5b2
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-08 07:12:14 -0700

    Time: 458 ms (9.66%), Space: 93.4 MB (26.8%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -12,24 +12,24 @@ public:
             if(i > idx && num[idx] == '0'){
                 break;
             }
-
             string s = num.substr(idx, i - idx + 1);
             long long val = stoll(s);
 
             if(idx == 0){
                 reversal(ans, num, target, s, val, val, i + 1);
             }else{
-                reversal(ans, num, target, move + "+" + s, curr + val, val, i + 1);
+                reversal(ans, num, target, move + '+' + s, curr + val, val, i + 1);
 
-                reversal(ans, num, target, move + "-" + s, curr - val, -val, i + 1);
+                reversal(ans, num, target, move + '-' + s, curr - val, -val, i + 1);
 
-                reversal(ans, num, target, move + "*" + s, curr - prev + prev * val, prev * val, i + 1);
+                reversal(ans, num, target, move + '*' + s, curr - prev + (prev * val), prev * val, i + 1);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
-        reversal(ans, num, target, "", 0, 0, 0);
+        string move = "";
+        reversal(ans, num, target, move, 0, 0, 0);
         return ans;
     }
 };
