823edca548ad8322b9757d79e1cda43235f9a7ab
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-08 13:20:35 -0700

    Time: 0 ms (100%), Space: 14 MB (94.11%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    void reversal(vector<vector<int>> &ans, vector<int> &curr, vector<int> &candidates, int target, int idx){
-       if(target == 0){
-        ans.push_back(curr);
-        return;
-       }
-
-       if(idx == candidates.size()){
-        return;
-       }
-
-        for(int i = idx; i < candidates.size(); i++){
-            if(i > idx && candidates[idx] == candidates[i]) continue;
-            if(candidates[i] > target) break;
-
-            curr.push_back(candidates[i]);
-            reversal(ans, curr, candidates, target - candidates[i], i);  
-            curr.pop_back();
+    void reversal(vector<vector<int>> &ans, vector<int> &candidates, int target, vector<int> &curr, int idx){
+        if(target == 0){
+            ans.push_back(curr);
         }
 
-
+        for(int i = idx; i < candidates.size(); i++){
+            if(candidates[i] > target) break;
+            curr.push_back(candidates[i]);
+            reversal(ans, candidates, target - candidates[i], curr, i);
+            curr.pop_back();
+        }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> curr;
-        reversal(ans, curr, candidates, target, 0);
+        reversal(ans, candidates, target, curr, 0);
         return ans;
     }
 };
