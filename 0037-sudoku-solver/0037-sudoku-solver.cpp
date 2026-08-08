314dc4c7252a26fc2c9de40588ef45d5c6789323
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-08 02:27:33 -0700

    Time: 429 ms (22.25%), Space: 8.8 MB (73.35%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
+    bool isChecker(vector<vector<char>> &board, char val, int row, int col){
+        for(int i = 0; i < 9; i++){
+            if(board[i][col] == val){
+                return false;
+            }
 
-    bool solve(vector<vector<char>> &board){
-        for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] == '.'){
-                    for(char c = '1'; c <= '9'; c++){
-                        if(isChecker(board, i, j, c)){
-                            board[i][j] = c;
-                            if(solve(board)){
+            if(board[row][i] == val){
+                return false;
+            }
+
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == val){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool reversal(vector<vector<char>> &board){
+        for(int row = 0; row < 9; row ++){
+            for(int col = 0; col < 9; col++){
+                if(board[row][col] == '.'){
+                    for(char i = '1'; i <= '9'; i++){
+                        if(isChecker(board, i, row, col)){
+                            board[row][col] = i;
+                            if(reversal(board)){
                                 return true;
-                            }else{
-                                board[i][j] = '.';
                             }
+                            board[row][col] = '.';
                         }
+                       
                     }
                      return false;
                 }
-                
-                 
             }
+              
         }
         return true;
-    }
-
-    bool isChecker(vector<vector<char>> &board, int row, int col, char c){
-        for(int i = 0; i < 9; i++){
-            if(board[row][i] == c){
-                return false;
-            }
-            if(board[i][col] == c){
-                return false;
-            }
-
-            if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == c){
-                return false;
-            }
-        }
-        return true;
+        
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        reversal(board);
+        return;
+        
     }
 };
