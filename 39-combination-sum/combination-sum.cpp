02662ae4d94d99fa5625e7205400c71d899aebe3
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-09 15:18:28 -0700

    Time: 0 ms (100.00%) | Memory: 14 MB (94.12%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void reversal(vector<vector<int>> &ans, vector<int> &candidates, int target, vector<int> &curr, int idx){
+    void reversal(vector<vector<int>> &ans, vector<int> &curr, vector<int> &candidates, int target, int idx){
         if(target == 0){
             ans.push_back(curr);
+            return;
         }
 
         for(int i = idx; i < candidates.size(); i++){
             if(candidates[i] > target) break;
             curr.push_back(candidates[i]);
-            reversal(ans, candidates, target - candidates[i], curr, i);
+            reversal(ans, curr, candidates, target - candidates[i], i);
             curr.pop_back();
         }
+
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
         vector<int> curr;
-        reversal(ans, candidates, target, curr, 0);
+        reversal(ans, curr, candidates, target, 0);
         return ans;
     }
 };
