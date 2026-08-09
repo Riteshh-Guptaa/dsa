58c44c774415e9fa5100acd8f9f1ab90f1aa816d
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-09 15:11:48 -0700

    Time: 434 ms (22.62%) | Memory: 93.5 MB (26.76%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void reversal(vector<string> &ans, string num, int target, string move, long long curr, long long prev, int idx){
+    void reversal(vector<string> &ans, string num, string move, int target, long long curr, long long prev, int idx){
         if(idx == num.size()){
             if(curr == target){
                 ans.push_back(move);
@@ -9,27 +9,23 @@ public:
         }
 
         for(int i = idx; i < num.size(); i++){
-            if(i > idx && num[idx] == '0'){
-                break;
-            }
-            string s = num.substr(idx, i - idx + 1);
-            long long val = stoll(s);
+            if(i > idx && num[idx] == '0') break;
+            string word = num.substr(idx, i - idx + 1);
+            long long val = stoll(word);
 
             if(idx == 0){
-                reversal(ans, num, target, s, val, val, i + 1);
+                reversal(ans, num, word, target, val, val, i + 1);
             }else{
-                reversal(ans, num, target, move + '+' + s, curr + val, val, i + 1);
-
-                reversal(ans, num, target, move + '-' + s, curr - val, -val, i + 1);
-
-                reversal(ans, num, target, move + '*' + s, curr - prev + (prev * val), prev * val, i + 1);
+                reversal(ans, num, move + '+' + word, target, curr + val, val, i + 1);
+                reversal(ans, num, move + '-' + word, target, curr - val, -val, i + 1);
+                reversal(ans, num, move + '*' + word, target, curr - prev + (prev * val), prev * val, i+1);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
         string move = "";
-        reversal(ans, num, target, move, 0, 0, 0);
+        reversal(ans, num, move, target, 0, 0, 0);
         return ans;
     }
 };
