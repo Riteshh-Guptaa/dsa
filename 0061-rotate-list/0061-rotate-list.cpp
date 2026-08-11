9c8edd60dc10929006628a992952fcbde43d6363
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-11 09:59:53 -0700

    Time: 0 ms (100%), Space: 16.5 MB (33.52%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -12,41 +12,39 @@ class Solution {
 public:
     int count(ListNode* head){
         int cnt = 0;
-        while(head){
+        while(head != NULL){
             cnt++;
             head = head->next;
         }
         return cnt;
     }
-
-    ListNode* pivot(ListNode* head, int req){
-        ListNode* curr = head;
-        int cnt = 1;
-        while(curr != NULL && cnt < req){
-            cnt++;
-            curr = curr->next;
-        }
-        return curr;
-    }
     ListNode* rotateRight(ListNode* head, int k) {
         if(head == NULL || head->next == NULL || k == 0){
             return head;
         }
-        
         int cnt = count(head);
+
         k %= cnt;
         if(k == 0) return head;
+
         int req = cnt - k;
-        ListNode* newNode = pivot(head, req);
-        ListNode* newHead = newNode->next;
-        newNode->next = NULL;
-        ListNode* temp = newHead;
-        while(temp->next){
+        ListNode* temp = head;
+        int i = 1;
+        while(temp != NULL && i < req){
             temp = temp->next;
+            i++;
         }
 
-        temp->next = head;
-        return newHead;
-        
+        ListNode* newHead = temp->next;
+        temp->next = NULL;
+        ListNode* dummy = newHead;
+        while(dummy->next){
+            dummy = dummy->next;
+        }
+        dummy->next = head;
+        head = newHead;
+        return head;
+
+
     }
 };
