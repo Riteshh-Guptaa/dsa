7a315779bbbc80c4af2e4a5091cdda7c3f6b0f5b
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-12 16:23:46 -0700

    Time: 475 ms (15.96%), Space: 9 MB (13.59%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.cpp
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -1,47 +1,42 @@
 class Solution {
 public:
-    bool isChecker(vector<vector<char>> &board, char val, int row, int col){
+    bool isCheck(vector<vector<char>> &board, char k, int row, int col){
         for(int i = 0; i < 9; i++){
-            if(board[i][col] == val){
+            if(board[row][i] == k){
                 return false;
             }
 
-            if(board[row][i] == val){
+            if(board[i][col] == k){
                 return false;
             }
 
-            if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == val){
+            if(board[3 * (row/3) + i/3][3 * (col/3) + i % 3] == k){
                 return false;
             }
         }
         return true;
     }
-
     bool reversal(vector<vector<char>> &board){
-        for(int row = 0; row < 9; row ++){
-            for(int col = 0; col < 9; col++){
-                if(board[row][col] == '.'){
-                    for(char i = '1'; i <= '9'; i++){
-                        if(isChecker(board, i, row, col)){
-                            board[row][col] = i;
+        for(int i = 0; i < board.size(); i++){
+            for(int j = 0; j < board[0].size(); j++){
+                if(board[i][j] == '.'){
+                    for(char k = '1'; k <= '9'; k++){
+                        if(isCheck(board, k, i, j)){
+                            board[i][j] = k;
                             if(reversal(board)){
                                 return true;
                             }
-                            board[row][col] = '.';
+                            board[i][j] = '.';
                         }
-                       
                     }
-                     return false;
+                    return false;
                 }
             }
-              
         }
         return true;
-        
     }
     void solveSudoku(vector<vector<char>>& board) {
         reversal(board);
         return;
-        
     }
 };
