bbf585f59a13dcc042739b4c704cf733ecfdebb1
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-12 13:11:07 -0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (31.25%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void reversal(vector<string> &ans, string curr, vector<string> &map, string digits, int idx){
+    void reversal(vector<string> &ans, string &curr, string digits, vector<string> &map, int idx){
         if(idx == digits.size()){
             ans.push_back(curr);
             return;
@@ -10,9 +10,10 @@ public:
 
         for(char letter : letters){
             curr.push_back(letter);
-            reversal(ans, curr, map, digits, idx + 1);
+            reversal(ans, curr, digits, map, idx + 1);
             curr.pop_back();
         }
+
     }
     vector<string> letterCombinations(string digits) {
         vector<string> map = {
@@ -27,11 +28,11 @@ public:
             "tuv",
             "wxyz"
         };
+
         vector<string> ans;
-        string curr = "";
-        reversal(ans, curr, map, digits, 0);
+        string curr;
+        reversal(ans, curr, digits, map, 0);
         return ans;
+
     }
-
-
 };
