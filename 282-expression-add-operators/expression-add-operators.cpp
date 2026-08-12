a52bbec2dc4ffaa332a5a747e19861b5046af683
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-12 16:53:44 -0700

    Time: 439 ms (18.83%) | Memory: 93.6 MB (21.29%) - LeetSync

--- 282-expression-add-operators/expression-add-operators.cpp
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    void reversal(vector<string> &ans, string num, string move, int target, long long curr, long long prev, int idx){
+    void reversal(vector<string> &ans, string move, string num, int target, int idx, long long curr, long long prev){
         if(idx == num.size()){
             if(curr == target){
                 ans.push_back(move);
             }
             return;
         }
-
         for(int i = idx; i < num.size(); i++){
             if(i > idx && num[idx] == '0') break;
-            string word = num.substr(idx, i - idx + 1);
-            long long val = stoll(word);
-
+            string newWord = num.substr(idx, i - idx + 1);
+            long long val = stoll(newWord);
             if(idx == 0){
-                reversal(ans, num, word, target, val, val, i + 1);
+                reversal(ans, newWord, num, target, i + 1, val, val);
             }else{
-                reversal(ans, num, move + '+' + word, target, curr + val, val, i + 1);
-                reversal(ans, num, move + '-' + word, target, curr - val, -val, i + 1);
-                reversal(ans, num, move + '*' + word, target, curr - prev + (prev * val), prev * val, i+1);
+                reversal(ans, move + '+' + newWord, num, target, i + 1, val + curr, val);
+                reversal(ans, move + '-' + newWord, num, target, i + 1, curr - val, -val);
+                reversal(ans, move + '*' + newWord, num, target, i + 1, curr - prev + (prev * val), prev * val);
             }
         }
     }
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
         string move = "";
-        reversal(ans, num, move, target, 0, 0, 0);
+        reversal(ans, move, num, target, 0, 0, 0);
         return ans;
     }
 };
