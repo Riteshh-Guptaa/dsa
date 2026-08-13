355893553ad9b56127a067b7eb5b89dca2a05050
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-14 03:19:56 +0530

    Time: 0 ms (100%), Space: 12.4 MB (35.45%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    void swipeGreater(vector<int> &arr1, vector<int> &arr2, int idx1, int idx2){
-        if(arr1[idx1] > arr2[idx2]){
-            swap(arr1[idx1], arr2[idx2]);
+    
+    void swipeGreater(vector<int> &arr1, vector<int> &arr2, int a1, int a2){
+        if(arr1[a1] > arr2[a2]){
+            swap(arr1[a1], arr2[a2]);
         }
     }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int len = n + m;
-        int gap = len/2 + len % 2;
-
-        while(gap > 0){
+        int req = (n + m + 1)/2;
+        while(req > 0){
             int l = 0;
-            int r = gap + l;
-            while(r < len){
+            int r = req + l;
+            while(r < n + m){
                 if(l < m && r < m){
                     swipeGreater(nums1, nums1, l, r);
                 }else if(l < m && r >= m){
                     swipeGreater(nums1, nums2, l, r - m);
                 }else{
-                    swipeGreater(nums2, nums2, l - m, r-m);
+                    swipeGreater(nums2, nums2, l - m, r - m);
                 }
                 l++;
+               
                 r++;
             }
-            if(gap <= 1) break;
-            gap = gap/2 + gap % 2;
+             if(req == 1){
+                break;
+            }
+            req = (req + 1)/2;
+           
         }
-       
-
         for(int i = 0; i < n; i++){
-            nums1[i + m] = nums2[i];
+           nums1[i + m] = nums2[i];
+           
         }
     }
 };
