96a89972022ecbe42fc78b2ed212efa0c01dc152
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-15 05:32:18 +0530

    Time: 35 ms (7.88%) | Memory: 70.5 MB (80.76%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    long long bouquets(vector<int>& bloomDay, int mid, int k){
+    long long bloomFinder(vector<int> &bloomDay, int k, int mid){
         long long cnt = 0;
         long long total = 0;
         for(int i = 0; i < bloomDay.size(); i++){
-            if(mid >= bloomDay[i]){
+            if(bloomDay[i] <= mid){
                 cnt++;
             }else{
                 total += cnt/k;
                 cnt = 0;
             }
-            
         }
         total += cnt/k;
-
-      
         return total;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int low = 0;
         int high = *max_element(bloomDay.begin(), bloomDay.end());
-        if((long long)m * k > bloomDay.size()) return -1;
+        if(bloomDay.size() < (long long)m * k) return -1;
         while(low <= high){
             int mid = low + (high - low)/2;
-
-            if(bouquets(bloomDay, mid, k) >= m){
+            if(bloomFinder(bloomDay, k, mid) >= m){
                 high = mid - 1;
             }else{
                 low = mid + 1;
