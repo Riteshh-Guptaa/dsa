d3a578197b274023f9e6a769bd9d16a80d94f7af
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-15 06:32:25 +0530

    Time: 15 ms (29.54%), Space: 35 MB (89%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    int capacity(vector<int> &weights, int mid){
-        int key = weights[0];
-        int cnt = 0;
+    int daysFinder(vector<int> &weights, int mid){
+        int cnt = 1;
+        int sum = weights[0];
         for(int i = 1; i < weights.size(); i++){
-            if(mid >= key + weights[i]){
-                key += weights[i];
+            if(mid >= sum + weights[i]){
+                sum += weights[i];
             }else{
                 cnt++;
-                key = weights[i];
+                sum = weights[i];
             }
         }
-        if(mid >= key) cnt++;
+
         return cnt;
     }
     int shipWithinDays(vector<int>& weights, int days) {
         int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 1);
-
+        int high = accumulate(weights.begin(), weights.end(), 0);
         while(low <= high){
             int mid = low + (high - low)/2;
-
-            if(capacity(weights, mid) > days){
-                low = mid + 1;
-            }else{
+            if(daysFinder(weights, mid) <= days){
                 high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
         return low;
