a794531e5636f0d628c2e719d9d1775a8ff35b6a
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-15 09:44:39 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.87%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-    int finder(vector<int> &nums, int mid){
+    int solver(vector<int> &nums, int mid){
         int cnt = 1;
         int sum = nums[0];
-
         for(int i = 1; i < nums.size(); i++){
-            if(mid >= sum + nums[i]){
+            if(nums[i] + sum <= mid){
                 sum += nums[i];
             }else{
                 cnt++;
@@ -16,16 +15,13 @@ public:
     }
     int splitArray(vector<int>& nums, int k) {
         int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end() , 0);
-
+        int high = accumulate(nums.begin(), nums.end(), 0);
         while(low <= high){
             int mid = low + (high - low)/2;
-
-
-            if(finder(nums, mid) > k){
-                low = mid + 1;
-            }else{
+            if(solver(nums, mid) <= k){
                 high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
         return low;
