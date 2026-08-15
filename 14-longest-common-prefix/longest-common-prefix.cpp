fca4437f657cf3e53dcc86fd383f4ae8a2766086
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-16 00:21:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (79.57%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,14 +3,14 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string ans = "";
         for(int i = 0; i < strs[0].size(); i++){
-            char c = strs[0][i];
+            char ch = strs[0][i];
             for(int j = 1; j < strs.size(); j++){
-                if(c != strs[j][i]){
+                if(i >= strs[j].size() || ch != strs[j][i]){
                     return ans;
                 }
-
+                
             }
-            ans += c;
+            ans += ch;
         }
         return ans;
     }
