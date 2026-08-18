0137dd87d129b6d97ae309f54825fdf19f7f273a
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-18 20:47:51 +0530

    Time: 4 ms (37.48%), Space: 16.7 MB (11.64%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -5,17 +5,19 @@ public:
         for(int i = 0; i < s.size(); i++){
             mp[s[i]]++;
         }
-        int n = s.size();
-        string ans = "";
-        vector<vector<char>> ch(n + 1);
+
+        vector<vector<char>> v(s.size() + 1);
+
         for(auto i : mp){
-            ch[i.second].push_back(i.first);
+            v[i.second].push_back(i.first);
         }
 
-        for(int i = n; i >= 1; i--){
-            for(char c : ch[i]){
+        string ans = "";
+
+        for(int i = s.size(); i >= 1; i--){
+            for(char ch : v[i]){
                 for(int j = 0; j < i; j++){
-                    ans += c;
+                    ans += ch;
                 }
             }
         }
