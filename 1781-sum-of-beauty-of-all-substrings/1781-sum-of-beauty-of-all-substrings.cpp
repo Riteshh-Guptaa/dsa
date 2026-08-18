aab5ffdbe10ec56d9a728342b26eceb25983ebd4
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-18 21:18:24 +0530

    Time: 104 ms (69.88%), Space: 11.1 MB (63.65%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -2,27 +2,23 @@ class Solution {
 public:
     int beautySum(string s) {
         int cnt = 0;
-        for(int i = 0; i < s.size(); i++){
-            vector<int> v(26, 0);
+        for(int i = 0;i < s.size(); i++){
+            vector<int> v(26);
             for(int j = i; j < s.size(); j++){
                 v[s[j] - 'a']++;
                 int mini = INT_MAX;
                 int maxi = INT_MIN;
-
-                for(int i = 0; i < 26; i++){
-                    if(v[i] >= 1){
-                        mini = min(mini, v[i]);
+                for(int k = 0; k < 26; k++){
+                    if(v[k] > 0){
+                        mini = min(v[k], mini);
                     }
-                    maxi = max(maxi, v[i]);
+                    maxi = max(v[k], maxi);
                 }
-
                 int beauty = maxi - mini;
                 cnt += beauty;
+
             }
-
-
         }
         return cnt;
-        
     }
 };
