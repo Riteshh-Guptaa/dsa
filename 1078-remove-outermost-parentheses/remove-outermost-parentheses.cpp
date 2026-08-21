5801a1367dba2fe9ec7eb86e27d8baec90c7f1f6
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-21 21:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (55.06%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -5,19 +5,17 @@ public:
         int depth = 0;
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '('){
-                if(depth > 0){
+                depth++;
+                if(depth > 1){
                     s[j++] = s[i];
                 }
-                depth++;
-            }
-            else{
+            }else{
                 depth--;
                 if(depth > 0){
                     s[j++] = s[i];
                 }
             }
         }
-
         s.resize(j);
         return s;
     }
