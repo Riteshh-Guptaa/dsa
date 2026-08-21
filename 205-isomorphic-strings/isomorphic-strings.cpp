7b763cd811a9978c4aa0002403771187d918af6f
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-21 23:56:41 +0530

    Time: 2 ms (48.62%) | Memory: 9.4 MB (30.71%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -4,20 +4,21 @@ public:
         unordered_map<char, char> mp1;
         unordered_map<char, char> mp2;
 
-        if(s.size() != t.size()) return false;
         for(int i = 0; i < s.size(); i++){
             char a = s[i];
             char b = t[i];
 
-            if(mp1.find(a) != mp1.end() && mp1[a] != b){
+            if(mp1.count(a) && mp1[a] != b){
                 return false;
             }
-            if(mp2.find(b) != mp2.end() && mp2[b] != a){
+
+            if(mp2.count(b) && mp2[b] != a){
                 return false;
             }
+
             mp1[a] = b;
             mp2[b] = a;
         }
         return true;
-    } 
+    }
 };
