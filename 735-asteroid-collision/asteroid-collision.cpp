789393362a1ae2a475f14d57430dd439a8b1f2f7
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-27 01:40:47 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (71.98%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> st;
+        for(int a : asteroids){
+            bool alive = true;
+            while(alive && !st.empty() && a < 0 && st.back() > 0){
+                if(st.back() < -a){
+                    st.pop_back();
+                }else if(st.back() == -a){
+                    st.pop_back();
+                    alive = false;
+                }else{
+                    alive = false;
+                }
+            } 
+            if(alive) st.push_back(a);
+        } 
+        return st;
+    }
+};
