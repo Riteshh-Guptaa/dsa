744e2197c2a2e63272959822242d4f06cbff6568
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-27 22:08:28 +0530

    Time: 0 ms (100%), Space: 11.6 MB (79.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,13 +6,13 @@ public:
         int high = nums.size() - 1;
         while(mid <= high){
             if(nums[mid] == 0){
-                swap(nums[low++], nums[mid++]);
+                swap(nums[mid++], nums[low++]);
             }else if(nums[mid] == 1){
                 mid++;
             }else{
                 swap(nums[mid], nums[high--]);
             }
         }
-        return;
+
     }
 };
