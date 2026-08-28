6f2440434339d7bb665f29a0a77ae9d9cb30cba3
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-28 11:35:18 +0530

    Time: 0 ms (100%), Space: 15.2 MB (68.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -8,21 +8,20 @@ public:
             if(nums[mid] == target){
                 return mid;
             }
-            if(nums[mid] >= nums[low]){
-                if(target >= nums[low] && nums[mid] > target){
+            if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && target < nums[mid]){
                     high = mid - 1;
                 }else{
                     low = mid + 1;
                 }
             }else{
-                if(target > nums[mid] && nums[high] >= target){
+                if(nums[mid] < target && target <= nums[high]){
                     low = mid + 1;
                 }else{
                     high = mid - 1;
                 }
             }
-        }  
-            return -1;
-        
+        }
+        return -1;
     }
 };
