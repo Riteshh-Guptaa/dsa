d5ec4b8d59bab5bd29589a064f6426387a4dd52c
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-29 11:36:14 +0530

    Time: 0 ms (100%), Space: 14.1 MB (80.03%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        int mini = INT_MAX;
         int low = 0;
         int high = nums.size() - 1;
-        int lowest = INT_MAX;
         while(low <= high){
             int mid = low + (high - low)/2;
             if(nums[low] <= nums[mid]){
-                
-                lowest = min(nums[low], lowest);
+                mini = min(nums[low], mini);
                 low = mid + 1;
-            }
-            else{
-               
-                lowest = min(lowest, nums[mid]);
-                 high = mid - 1;
+            }else{
+                mini = min(nums[mid], mini);
+                high = mid - 1;
             }
         }
-        return lowest;
+        return mini;
     }
 };
