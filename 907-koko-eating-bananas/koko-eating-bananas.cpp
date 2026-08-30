7c21531c40b93158e2071a9ae0957b0fdffc6bce
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-30 08:38:53 +0530

    Time: 11 ms (35.15%) | Memory: 22.9 MB (58.08%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,6 +1,6 @@
 class Solution {
-public:  
-    long long timeFinder(vector<int> &piles, int mid){
+public:
+    long long bananaEaten(vector<int> &piles, int mid){
         long long cnt = 0;
         for(int i = 0; i < piles.size(); i++){
             cnt += ceil((double)piles[i]/mid);
@@ -15,10 +15,10 @@ public:
         while(low <= high){
             int mid = low + (high - low)/2;
 
-            if(timeFinder(piles, mid) <= h){
-                high = mid - 1;
-            }else{
+            if(bananaEaten(piles, mid) > h){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
         return low;
