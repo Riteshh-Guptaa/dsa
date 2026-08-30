3fc85851dfadc2064473ef12104a222e3287c93f
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-31 01:55:42 +0530

    Time: 17 ms (13.95%), Space: 34.9 MB (88.5%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    int daysFinder(vector<int> &weights, int mid){
+    int finder(vector<int> &weights, int mid){
         int cnt = 1;
         int sum = weights[0];
         for(int i = 1; i < weights.size(); i++){
-            if(mid >= sum + weights[i]){
+            if(sum + weights[i] <= mid){
                 sum += weights[i];
             }else{
                 cnt++;
@@ -17,14 +17,16 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int low = *max_element(weights.begin(), weights.end());
         int high = accumulate(weights.begin(), weights.end(), 0);
+
         while(low <= high){
             int mid = low + (high - low)/2;
-            if(daysFinder(weights, mid) <= days){
+            if(finder(weights, mid) <= days){
                 high = mid - 1;
             }else{
                 low = mid + 1;
             }
         }
         return low;
+        
     }
 };
