0a9a50511138cc95bc280a1e6f081af3c5a76409
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-08-31 12:54:13 +0530

    Time: 2 ms (48.18%), Space: 9.3 MB (58.2%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -3,19 +3,15 @@ public:
     bool isIsomorphic(string s, string t) {
         unordered_map<char, char> mp1;
         unordered_map<char, char> mp2;
-
         for(int i = 0; i < s.size(); i++){
             char a = s[i];
             char b = t[i];
-
-            if(mp1.count(a) && mp1[a] != b){
+            if(mp1.find(a) != mp1.end() && mp1[a] != b){
                 return false;
             }
-
-            if(mp2.count(b) && mp2[b] != a){
+            if(mp2.find(b) != mp2.end() && mp2[b] != a){
                 return false;
             }
-
             mp1[a] = b;
             mp2[b] = a;
         }
