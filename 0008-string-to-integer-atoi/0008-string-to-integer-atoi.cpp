828c4cf24bfe4e6e201e8fe7ca8953ef85534438
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-01 13:48:00 +0530

    Time: 0 ms (100%), Space: 9.1 MB (81.61%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
-    int recursion(string s, int i, int sign, int ans){
-        if(i == s.size() || !isdigit(s[i])){
-            return sign * ans;
-        }
-
-        int digit = s[i] - '0';
-        if(ans > INT_MAX/10 || ans == INT_MAX/10 && digit > 7){
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-        ans = ans * 10 + digit;
-        return recursion(s, i + 1, sign, ans);
-    }
     int myAtoi(string s) {
         int i = 0;
         while(i < s.size() && s[i] == ' ') i++;
-        if(i >= s.size()) return 0;
         int sign = 1;
         if(s[i] == '+' || s[i] == '-'){
             if(s[i] == '-'){
@@ -23,7 +10,20 @@ public:
             }
             i++;
         }
-        int ans = 0;
-        return recursion(s, i, sign, ans);
+        long long ans = 0;
+
+        while(i < s.size() && isdigit(s[i])){
+            ans = ans * 10 + (s[i] - '0');
+
+            if(sign == 1 && ans > INT_MAX){
+                return INT_MAX;
+            }
+
+            if(sign == -1 && -ans < INT_MIN){
+                return INT_MIN;
+            }
+            i++;
+        }
+        return sign * ans;
     }
 };
