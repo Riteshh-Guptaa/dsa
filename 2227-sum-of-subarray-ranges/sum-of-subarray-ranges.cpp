dd504367805ccb255c2e3a351a68619b1dfbb7f2
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-03 04:37:55 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (72.57%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,52 +1,44 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
+        stack<int> st;
         int n = nums.size();
         long long ans = 0;
-        stack<int> st;
-
         for(int i = 0; i <= n; i++){
             
-            while(!st.empty() && (i == n || nums[st.top()] >= nums[i])){
+            while(!st.empty() && ( i == n || nums[st.top()] >= nums[i])){
                 int mid = st.top();
                 st.pop();
 
                 int left = st.empty() ? -1 : st.top();
                 int right = i;
+                int leftIdx = mid - left;
+                int rightIdx = right - mid;
 
-                long long leftCount = mid - left;
-                long long rightCount = right - mid;
-                ans -= (long long)nums[mid] * leftCount * rightCount;
+                ans -= (long long)nums[mid] * leftIdx * rightIdx;
             }
             if(i < n){
                 st.push(i);
             }
         }
-
-        while(!st.empty()){
-            st.pop();
-        }
-
+        while(!st.empty()) st.pop();
+        
         for(int i = 0; i <= n; i++){
-            int cur = (i == n) ? 0 : nums[i];
-
-            while(!st.empty() && ( i == n || nums[st.top()] <= nums[i])){
+            int cur = (i == n) ? -1 : nums[i];
+            while(!st.empty() && (i == n || nums[st.top()] <= nums[i])){
                 int mid = st.top();
                 st.pop();
 
                 int left = st.empty() ? -1 : st.top();
                 int right = i;
-                int leftCount = mid - left;
-                int rightCount = right - mid;
-
-                ans += (long long)nums[mid] * leftCount * rightCount;
+                int leftIdx = mid - left;
+                int rightIdx = right - mid;
+                ans += (long long)nums[mid] * leftIdx * rightIdx;
             }
-             if(i < n){
-            st.push(i);
-           }
+            if(i < n){
+                st.push(i);
+            }
         }
         return ans;
-
-
     }
 };
