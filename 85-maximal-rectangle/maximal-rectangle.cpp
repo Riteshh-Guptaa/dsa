9bb041d0ba123b12a7fb2cb4a919188a29621a12
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-03 12:30:26 +0530

    Time: 11 ms (39.63%) | Memory: 19.3 MB (64.38%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int> &heights){
+    int ansFinder(vector<int> &height){
+        int n = height.size();
+        int maxi = 0;
         stack<int> st;
-        int n = heights.size();
-        int ans = 0;
         for(int i = 0; i <= n; i++){
-            int cur = (i == n) ? 0 : heights[i];
-            while(!st.empty() && heights[st.top()] >= cur){
+            int cur = (i == n) ? -1 : height[i];
+            while(!st.empty() && height[st.top()] > cur){
                 int mid = st.top();
                 st.pop();
-
                 int left = st.empty() ? -1 : st.top();
-                int width = i - left - 1;
+                int right = i;
+                long long area = 1LL * height[mid] * (right - left - 1);
 
-                ans = max(ans, heights[mid] * width);
+                maxi = max((int)area, maxi); 
             }
             if(i < n){
                 st.push(i);
             }
         }
-        return ans;
-
+        return maxi;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
-
-        vector<int> heights(col, 0);
-        int ans = 0;
+        vector<int> v(col, 0);
+        int maxi = 0;
 
         for(int i = 0; i < row; i++){
             for(int j = 0; j < col; j++){
                 if(matrix[i][j] == '1'){
-                    heights[j]++;
+                    v[j]++;
                 }else{
-                    heights[j] = 0;
+                    v[j] = 0;
                 }
+               
             }
-            ans = max(ans, largestRectangleArea(heights));
+             maxi = max(maxi, ansFinder(v));
         }
-        return ans;
-        
+        return maxi;
     }
 };
