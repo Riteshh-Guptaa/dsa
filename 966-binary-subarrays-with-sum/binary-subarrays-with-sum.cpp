50e6625becf670114b8656105376fbd0c580e7e3
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-07 13:09:21 +0530

    Time: 0 ms (100.00%) | Memory: 33.9 MB (36.77%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+         int n = nums.size();
+
+        vector<int> freq(n + 1, 0);
+        freq[0] = 1;
+
+        int sum = 0;
+        int ans = 0;
+
+        for (int num : nums) {
+            sum += num;
+
+            if (sum - goal >= 0) {
+                ans += freq[sum - goal];
+            }
+
+            freq[sum]++;
+        }
+
+        return ans;
+    }
+};
