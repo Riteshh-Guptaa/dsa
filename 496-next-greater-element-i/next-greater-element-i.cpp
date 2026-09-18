c86b331464bb4b595926c35fab18574ef3cc089d
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-18 21:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (38.22%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,6 +3,7 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         unordered_map<int, int> mpp;
         stack<int> st;
+
         for(int i = nums2.size() - 1; i >= 0; i--){
             while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
@@ -16,8 +17,8 @@ public:
 
         for(int i = 0; i < nums1.size(); i++){
             nums1[i] = mpp[nums1[i]];
+            
         }
-
         return nums1;
     }
 };
