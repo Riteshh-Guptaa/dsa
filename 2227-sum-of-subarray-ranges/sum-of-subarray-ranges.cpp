9cc8a75db605e25bc409f26d806ff8c0de9277b3
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-20 00:19:52 +0530

    Time: 4 ms (59.87%) | Memory: 14.1 MB (71.32%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
-        stack<int> st;
         int n = nums.size();
+        stack<int> st;
         long long ans = 0;
         for(int i = 0; i <= n; i++){
-            
-            while(!st.empty() && ( i == n || nums[st.top()] >= nums[i])){
+            while(!st.empty() && (i == n || nums[st.top()] >= nums[i])){
                 int mid = st.top();
                 st.pop();
 
@@ -14,26 +13,31 @@ public:
                 int right = i;
                 int leftIdx = mid - left;
                 int rightIdx = right - mid;
-
                 ans -= (long long)nums[mid] * leftIdx * rightIdx;
+
             }
             if(i < n){
                 st.push(i);
             }
         }
-        while(!st.empty()) st.pop();
-        
+
+        while(!st.empty()){
+            st.pop();
+        }
+
+
         for(int i = 0; i <= n; i++){
-            int cur = (i == n) ? -1 : nums[i];
             while(!st.empty() && (i == n || nums[st.top()] <= nums[i])){
                 int mid = st.top();
                 st.pop();
 
-                int left = st.empty() ? -1 : st.top();
+
+                int left = st.empty() ? -1 : st.top(); 
                 int right = i;
                 int leftIdx = mid - left;
                 int rightIdx = right - mid;
                 ans += (long long)nums[mid] * leftIdx * rightIdx;
+
             }
             if(i < n){
                 st.push(i);
