0e1127eb3461f95ce024c1a08a7da1a832d141cc
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-22 22:18:56 +0530

    Time: 54 ms (69.71%) | Memory: 151.3 MB (38.42%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -3,21 +3,22 @@ class MinStack {
     stack<int> mn;
 public:
     MinStack() {
+        
     }
     
     void push(int value) {
         st.push(value);
-
         if(mn.empty() || value <= mn.top()){
             mn.push(value);
         }
+
     }
     
     void pop() {
         if(st.top() == mn.top()){
             mn.pop();
         }
-        st.pop();
+        return st.pop();
     }
     
     int top() {
