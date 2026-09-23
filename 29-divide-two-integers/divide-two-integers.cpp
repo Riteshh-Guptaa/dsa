160dcc68a1611b007b2640d9178a08609fef6af9
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-23 21:58:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (39.33%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -3,6 +3,7 @@ public:
     int divide(int dividend, int divisor) {
         if(dividend == INT_MIN && divisor == -1){
             return INT_MAX;
+
         }
 
         long long a = abs((long long) dividend);
@@ -10,15 +11,16 @@ public:
         long long ans = 0;
 
         for(int i = 31; i >= 0; i--){
-            if((b << i) <= a){
+            if(a >= (b << i)){
                 a -= (b << i);
-                ans += (1 << i);
+                ans |= (1 << i);
             }
+
         }
 
-        if((divisor < 0) ^ (dividend < 0)){
+        if(dividend < 0 ^ divisor < 0){
             ans = -ans;
         }
-        return (int)ans;
+        return ans;
     }
 };
