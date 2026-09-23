9b3c9142752653719432115a0311083f3cf79ec2
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-23 14:00:12 +0530

    Time: 8 ms (54.24%) | Memory: 19.4 MB (61.97%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,42 +1,40 @@
 class Solution {
 public:
-    int ansFinder(vector<int> &height){
-        int n = height.size();
-        int maxi = 0;
+    int finder(vector<int> &nums){
+        int maxxi = 0;
         stack<int> st;
+        int n = nums.size();
         for(int i = 0; i <= n; i++){
-            int cur = (i == n) ? -1 : height[i];
-            while(!st.empty() && height[st.top()] > cur){
+            int curr = (i == n) ? -1 : nums[i];
+            while(!st.empty() && nums[st.top()] > curr){
                 int mid = st.top();
                 st.pop();
+
                 int left = st.empty() ? -1 : st.top();
                 int right = i;
-                long long area = 1LL * height[mid] * (right - left - 1);
-
-                maxi = max((int)area, maxi); 
+                int area = (long long)nums[mid] * (right - left - 1);
+                maxxi = max(maxxi, area);
             }
             if(i < n){
                 st.push(i);
             }
         }
-        return maxi;
+        return maxxi;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
-        vector<int> v(col, 0);
+        int n = matrix.size();
+        int m = matrix[0].size();
         int maxi = 0;
-
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
-                if(matrix[i][j] == '1'){
-                    v[j]++;
-                }else{
-                    v[j] = 0;
-                }
-               
+        vector<int> v(m);
+        for(int i = 0; i < n; i++){
+           for(int j = 0; j < m; j++){
+            if(matrix[i][j] == '1'){
+                v[j]++;
+            }else{
+                v[j] = 0;
             }
-             maxi = max(maxi, ansFinder(v));
+           }
+           maxi = max(maxi, finder(v));
         }
         return maxi;
     }
