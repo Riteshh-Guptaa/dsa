106f2e16c99b70ba247e7a35e076524ab43035a4
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-24 18:21:43 +0530

    Time: 9 ms (46.55%) | Memory: 19.2 MB (69.51%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.cpp
+++ 85-maximal-rectangle/maximal-rectangle.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-    int finder(vector<int> &nums){
-        int maxxi = 0;
+    int ansFinder(vector<int> &nums){
         stack<int> st;
         int n = nums.size();
+        int maxxi = 0;
         for(int i = 0; i <= n; i++){
             int curr = (i == n) ? -1 : nums[i];
             while(!st.empty() && nums[st.top()] > curr){
@@ -12,8 +12,9 @@ public:
 
                 int left = st.empty() ? -1 : st.top();
                 int right = i;
-                int area = (long long)nums[mid] * (right - left - 1);
-                maxxi = max(maxxi, area);
+
+                int ans = (long long)nums[mid] * (right - left - 1);
+                maxxi = max(ans, maxxi);
             }
             if(i < n){
                 st.push(i);
@@ -24,17 +25,18 @@ public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        int maxi = 0;
         vector<int> v(m);
+        int maxi = 0;
+
         for(int i = 0; i < n; i++){
-           for(int j = 0; j < m; j++){
-            if(matrix[i][j] == '1'){
-                v[j]++;
-            }else{
-                v[j] = 0;
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == '1'){
+                    v[j]++;
+                }else{
+                    v[j] = 0;
+                }
             }
-           }
-           maxi = max(maxi, finder(v));
+            maxi = max(maxi, ansFinder(v));
         }
         return maxi;
     }
