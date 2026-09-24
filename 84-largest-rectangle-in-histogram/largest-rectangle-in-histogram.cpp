0bd633a103e359d666882e153a8b2cd843970ce4
Ritesh Gupta <sanjayrinki1984@gmail.com>
2026-09-24 18:15:39 +0530

    Time: 27 ms (37.92%) | Memory: 81.4 MB (61.13%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -4,15 +4,15 @@ public:
         stack<int> st;
         int n = heights.size();
         int maxi = 0;
-        for(int i = 0; i <= n; i++){
+        for(int i = 0; i <= heights.size(); i++){
             int curr = (i == n) ? -1 : heights[i];
             while(!st.empty() && heights[st.top()] > curr){
                 int mid = st.top();
                 st.pop();
                 int left = st.empty() ? -1 : st.top();
                 int right = i;
-                long long area = (long long)heights[mid] * (right - left - 1);
-                maxi = max(maxi, (int)area);
+                int ans = (long long)heights[mid] * (right - left - 1);
+                maxi = max(ans, maxi);
             }
             if(i < n){
                 st.push(i);
